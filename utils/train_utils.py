5f4a55d7be9defc9db47e35ce0a523efce5c0d5f
mochew <40199772+mochew@users.noreply.github.com>
2026-01-28 18:52:44 +0800

    Clean up print statements in remove_on_target function
    Removed print statements for cleaner output.

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -5,18 +5,15 @@ def remove_on_target(train_data, inter_Val_data, cross_Val_data):
     matches = [1, 7, 13, 19, 25, 31]
     print("remove on target")
     train_On_Target_mask = np.array([any(x not in matches for x in arr[:-3]) for arr in train_data[0]])
-    print("train")
+
     train_Pos_list = train_data[0][train_On_Target_mask]
     train_labels = train_data[1][train_On_Target_mask]
 
-    print("inter_Val")
     inter_Val_On_Target_mask = np.array([any(x not in matches for x in arr[:-3]) for arr in inter_Val_data[0]])
     inter_Val_Pos_list = inter_Val_data[0][inter_Val_On_Target_mask]
     inter_Val_labels = inter_Val_data[1][inter_Val_On_Target_mask]
     inter_Sg_list = inter_Val_data[2][inter_Val_On_Target_mask]
 
-
-    print("cross_Val")
     cross_Val_On_Target_mask = np.array([any(x not in matches for x in arr[:-3]) for arr in cross_Val_data[0]])
     cross_Val_Pos_list = cross_Val_data[0][cross_Val_On_Target_mask]
     cross_Val_labels = cross_Val_data[1][cross_Val_On_Target_mask]
